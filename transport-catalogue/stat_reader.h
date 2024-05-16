4156f840fd7d581bdeb63e612c29b0972151df6d
nickyul <156240952+nickyul@users.noreply.github.com>
2024-05-16 16:48:12 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,17 @@
-// место для вашего кода
+#pragma once
+#include <cmath>
+#include <iosfwd>
+
+#include "transport_catalogue.h"
+
+namespace catalogue {
+    namespace output {
+
+        void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
+            std::ostream& output);
+
+        void StopStatPrint(const std::set<std::string>& buses_on_stop, std::string_view request_id, std::ostream& output);
+
+        void BusStatPrint(const Bus& bus, std::ostream& output);
+    }
+}
