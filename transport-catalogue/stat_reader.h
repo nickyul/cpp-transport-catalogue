6d439d717994279dbc98e9952c53cedda5d29a15
nickyul <156240952+nickyul@users.noreply.github.com>
2024-05-17 23:18:57 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -9,9 +9,5 @@ namespace catalogue {
 
         void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
             std::ostream& output);
-
-        void StopStatPrint(const std::set<std::string>& buses_on_stop, std::string_view request_id, std::ostream& output);
-
-        void BusStatPrint(const Bus& bus, std::ostream& output);
     }
 }
