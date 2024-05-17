cdb2562d4f2c2554e4b1681555ef52520eb8bebc
nickyul <156240952+nickyul@users.noreply.github.com>
2024-05-17 23:18:45 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -4,82 +4,55 @@
 using namespace std::string_literals;
 using namespace catalogue;
 
-struct unique_names_hasher {
-	size_t operator()(const std::string_view& stop_name) const {
-		double hash = 0;
-		for (int i = 0; i < stop_name.size(); ++i) {
-			hash += stop_name[i] * std::pow(37, i);
-		}
-		return static_cast<size_t>(hash);
-	}
-};
-
 void output::ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
-    std::ostream& output) {
-    auto colon_space = request.find(' ');
-    if (colon_space == request.npos) {
+	std::ostream& output) {
+	auto colon_space = request.find(' ');
+	if (colon_space == request.npos) {
 		output << "Can't read the request! \n"s;
-        return;
-    }
-	
+		return;
+	}
+
 	std::string_view command = request.substr(0, colon_space);
 	std::string_view request_id = request.substr(colon_space + 1);
 
 	if (command == "Stop"s) {
-		std::set<std::string> buses_on_stop;
-		if (transport_catalogue.RequestStop(buses_on_stop, request_id)) {
-			output::StopStatPrint(buses_on_stop, request_id, output);
+		
+		const Stop* stop = transport_catalogue.GetStop(request_id);
+		if (stop == nullptr) {
+			output << "Stop "s << request_id << ": not found\n"s;
+			return;
+		}
+		std::unordered_set<const Bus*> buses_on_stop = transport_catalogue.RequestStop(stop);
+		if (buses_on_stop.empty()) {
+			output << "Stop "s << request_id << ": no buses\n"s;
+			return;
 		}
 		else {
-			output << "Stop "s << request_id << ": not found \n"s;
+			std::vector<std::string> sort_bus;
+			for (const Bus* bus : buses_on_stop) {
+				sort_bus.push_back(bus->bus_name);
+			}
+			std::sort(sort_bus.begin(), sort_bus.end());
+			output << "Stop "s << request_id << ": buses ";
+			for (const std::string_view s_bus : sort_bus) {
+				output << s_bus << " "s;
+			}
+			output << "\n";
 		}
 	}
 
 	else if (command == "Bus"s) {
-		Bus bus = transport_catalogue.RequestBus(request_id);
-		if (bus.stops.empty()) {
+		BusStat stats = transport_catalogue.RequestBus(request_id);
+		if (!stats.route_length) {
 			output << "Bus "s << request_id << ": not found \n"s;
 			return;
 		}
-		output::BusStatPrint(bus, output);
+		output << "Bus "s << request_id << ": "s << stats.total_stops << " stops on route, "s << 
+			stats.unique_stops << " unique stops, "s << std::setprecision(6) << stats.route_length << " route length\n";
 	}
 
 	else {
-		output << "Can't read the command! \n"s;
+		output << "Can't read the command!\n"s;
 		return;
 	}
-
-}
-
-
-void output::StopStatPrint(const std::set<std::string>& buses_on_stop, std::string_view request_id, std::ostream& output) {
-	if (buses_on_stop.empty()) {
-		output << "Stop "s << request_id << ": no buses \n"s;
-		return;
-	}
-	output << "Stop "s << request_id << ": buses ";
-	for (std::string_view bus : buses_on_stop) {
-		output << bus << " "s;
-	}
-	output << "\n";
-}
-
-void output::BusStatPrint(const Bus& bus, std::ostream& output) {
-	double distance = 0;
-	std::unordered_set<std::string_view, unique_names_hasher> unique_stops_names;
-
-	// Compute distance
-	detail::Coordinates previous_coord{ bus.stops[0]->lat, bus.stops[0]->lng };
-	bool first_iter = true;
-	for (const Stop* stop : bus.stops) {
-		if (first_iter) {
-			first_iter = false;
-			unique_stops_names.insert(stop->stop_name);
-			continue;
-		}
-		distance += detail::ComputeDistance(previous_coord, detail::Coordinates{ stop->lat, stop->lng });
-		previous_coord = { stop->lat, stop->lng };
-		unique_stops_names.insert(stop->stop_name);
-	}
-	output << "Bus "s << bus.bus_name << ": "s << bus.stops.size() << " stops on route, "s << unique_stops_names.size() << " unique stops, "s << std::setprecision(6) << distance << " route length \n";
 }
