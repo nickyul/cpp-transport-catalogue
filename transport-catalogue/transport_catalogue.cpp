4326c615c41d9a8ad1ec057e04cc650711564933
nickyul <156240952+nickyul@users.noreply.github.com>
2024-06-30 11:49:07 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -2,33 +2,23 @@
 
 using namespace catalogue;
 
-struct unique_names_hasher {
-	size_t operator()(const std::string_view& stop_name) const {
-		double hash = 0;
-		for (int i = 0; i < stop_name.size(); ++i) {
-			hash += stop_name[i] * std::pow(37, i);
-		}
-		return static_cast<size_t>(hash);
-	}
-};
-
 void TransportCatalogue::AddStop(const std::string& stop_name, const detail::Coordinates& coordinates) {
 	Stop stop{ stop_name, coordinates.lat, coordinates.lng };
-	stops_.push_back(std::move(stop));
+	stops_.emplace_back(std::move(stop));
 	stopname_to_stop_[stops_.back().stop_name] = &stops_.back();
 }
 
-void TransportCatalogue::AddDistance(const Stop* stop_from, const Stop* stop_to, int distance) {
+void TransportCatalogue::AddDistance(StopPtr stop_from, StopPtr stop_to, int distance) {
 	pair_stop_to_distance_[{stop_from, stop_to}] = distance;
 }
 
 
-void TransportCatalogue::AddBus(const std::string& bus_name, const std::vector<const Stop*> stops) {
-	const Bus bus{ bus_name, std::move(stops) };
-	buses_.push_back(std::move(bus));
+void TransportCatalogue::AddBus(const std::string& bus_name, const std::vector<StopPtr> stops, bool is_roundtrip) {
+	const Bus bus{ bus_name, std::move(stops), is_roundtrip };
+	buses_.emplace_back(std::move(bus));
 	busname_to_bus_[buses_.back().bus_name] = &buses_.back();
 
-	for (const Stop* stop : buses_.back().stops) {
+	for (StopPtr stop : buses_.back().stops) {
 		bus_by_stop_[stop].insert(&buses_.back());
 	}
 }
@@ -36,16 +26,16 @@ void TransportCatalogue::AddBus(const std::string& bus_name, const std::vector<c
 BusStat TransportCatalogue::RequestBus(std::string_view bus_name) const {
 	// if bus exist
 	if (busname_to_bus_.count(bus_name)) {
-		const Bus* bus = busname_to_bus_.at(bus_name);
+		BusPtr bus = busname_to_bus_.at(bus_name);
 		BusStat stats;
 		std::unordered_set<std::string_view, unique_names_hasher> unique_stops_names;
 
 		double geo_distance = 0;
 		// Compute distance
 		detail::Coordinates previous_coord{ bus->stops[0]->coordinates };
-		const Stop* previous_stop = bus->stops[0];
+		StopPtr previous_stop = bus->stops[0];
 		bool first_iter = true;
-		for (const Stop* stop : bus->stops) {
+		for (StopPtr stop : bus->stops) {
 			if (first_iter) {
 				first_iter = false;
 				unique_stops_names.insert(stop->stop_name);
@@ -65,34 +55,42 @@ BusStat TransportCatalogue::RequestBus(std::string_view bus_name) const {
 	return {};
 }
 
-std::unordered_set<const Bus*> TransportCatalogue::RequestStop(const Stop* stop) const {
+const std::unordered_set<BusPtr>* TransportCatalogue::RequestStop(StopPtr stop) const {
 	if (bus_by_stop_.count(stop)) {
-		return bus_by_stop_.at(stop);
+		const std::unordered_set<BusPtr>* buses_by_stop = &bus_by_stop_.at(stop);
+		return buses_by_stop;
 	}
-	return {};
+	return nullptr;
 }
 
-const Stop* TransportCatalogue::GetStop(std::string_view stop_name) const {
+StopPtr TransportCatalogue::GetStop(std::string_view stop_name) const {
 	if (stopname_to_stop_.count(stop_name)) {
 		return stopname_to_stop_.at(stop_name);
 	}
 	return nullptr;
 }
 
-const Bus* TransportCatalogue::GetBus(std::string_view bus_name) const {
+BusPtr TransportCatalogue::GetBus(std::string_view bus_name) const {
 	if (busname_to_bus_.count(bus_name)) {
 		return busname_to_bus_.at(bus_name);
 	}
 	return nullptr;
 }
 
-int TransportCatalogue::GetDistance(const Stop* stop_from, const Stop* stop_to) const {
-	if (pair_stop_to_distance_.count({stop_from, stop_to})) {
+const std::unordered_map<StopPtr, std::unordered_set<BusPtr>>* catalogue::TransportCatalogue::GetBusesByStop() const {
+	return &bus_by_stop_;
+}
+
+const BusMap* catalogue::TransportCatalogue::GetBusMap() const {
+	return &busname_to_bus_;
+}
+
+int TransportCatalogue::GetDistance(StopPtr stop_from, StopPtr stop_to) const {
+	if (pair_stop_to_distance_.count({ stop_from, stop_to })) {
 		return pair_stop_to_distance_.at({ stop_from, stop_to });
 	}
 	if (pair_stop_to_distance_.count({ stop_to, stop_from })) {
 		return pair_stop_to_distance_.at({ stop_to, stop_from });
 	}
-
 	return 0;
 }
