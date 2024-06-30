78065127a357d8abde8c591e54735093fe5c4dbb
nickyul <156240952+nickyul@users.noreply.github.com>
2024-06-30 11:48:31 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
 #include <string>
 
-#include "input_reader.h"
-#include "stat_reader.h"
+#include "request_handler.h"
+#include "map_renderer.h"
+#include "json_reader.h"
 
 using namespace std;
 using namespace catalogue;
+using namespace json;
 
 int main() {
     TransportCatalogue catalogue;
+    JsonReader json_reader(cin);
+    json_reader.MakeCatalogue(catalogue);
+    RenderSettings settings = json_reader.ParseSettings();
 
-    int base_request_count;
-    cin >> base_request_count >> ws;
-
-    {
-        input::InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(cin, line);
-            reader.ParseLine(line);
-        }
-        reader.ApplyCommands(catalogue);
-    }
+    RequestHandler request_handler(catalogue, json_reader, settings);
     
-    int stat_request_count;
-    cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(cin, line);
-        output::ParseAndPrintStat(catalogue, line, cout);
-    }
-   
+    Document doc_out = request_handler.GetRequestDocument();
+
+    Print(doc_out, cout);
+
     return 0;
 }
