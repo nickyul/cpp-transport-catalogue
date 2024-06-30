fc66605cae6a49962194b126a8b37524a638bf97
nickyul <156240952+nickyul@users.noreply.github.com>
2024-06-30 11:49:27 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -4,52 +4,45 @@
 #include <unordered_set>
 #include <set>
 #include <string>
+#include <string_view>
 #include <deque>
 #include <vector>
 
 #include "geo.h"
+#include "domain.h"
 
 
 namespace catalogue {
 
-	struct Stop {
-		std::string stop_name;
-		detail::Coordinates coordinates;
-	};
-
-	struct Bus {
-		std::string bus_name;
-		std::vector<const Stop*> stops;
-	};
-
-	struct BusStat {
-		size_t total_stops = 0;
-		size_t unique_stops = 0;
-		double route_length = 0.;
-		double curvature = 0;
-	};
-
 	struct pair_stops_hasher {
 		size_t operator()(const std::pair<const Stop*, const Stop*>& pair) const {
 			std::hash<const void*> hasher;
-			return hasher(pair.first) + hasher(pair.second)*37;
+			return hasher(pair.first) + hasher(pair.second) * 37;
 		}
 	};
 
-	using StopMap = std::unordered_map<std::string_view, const Stop*>;
-	using BusMap = std::unordered_map<std::string_view, const Bus*>;
+	struct unique_names_hasher {
+		size_t operator()(const std::string_view& stop_name) const {
+			double hash = 0;
+			for (size_t i = 0; i < stop_name.size(); ++i) {
+				hash += stop_name[i] * std::pow(37, i);
+			}
+			return static_cast<size_t>(hash);
+		}
+	};
 
 	class TransportCatalogue {
 	public:
 		void AddStop(const std::string& stop_name, const detail::Coordinates& coordinates);
-		void AddDistance(const Stop* stop_from, const Stop* stop_to, int distance);
-		void AddBus(const std::string& bus_name, const std::vector<const Stop*> stops);
+		void AddDistance(StopPtr stop_from, StopPtr stop_to, int distance);
+		void AddBus(const std::string& bus_name, const std::vector<StopPtr> stops, bool is_roundtrip);
 		BusStat RequestBus(std::string_view bus_name) const;
-		// возвращайте константный указатель на объект по имени остановки
-		std::unordered_set<const Bus*> RequestStop(const Stop* stop) const;
-		const Stop* GetStop(std::string_view stop_name) const;
-		const Bus* GetBus(std::string_view bus_name) const;
-		int GetDistance(const Stop* stop_from, const Stop* stop_to) const;
+		const std::unordered_set<BusPtr>* RequestStop(StopPtr stop) const;
+		StopPtr GetStop(std::string_view stop_name) const;
+		BusPtr GetBus(std::string_view bus_name) const;
+		const std::unordered_map<StopPtr, std::unordered_set<BusPtr>>* GetBusesByStop() const;
+		const BusMap* GetBusMap() const;
+		int GetDistance(StopPtr stop_from, StopPtr stop_to) const;
 
 	private:
 		// deque всех остановок
@@ -61,8 +54,8 @@ namespace catalogue {
 		// мапа [название маршрута] = указатель на маршрут в деке
 		BusMap busname_to_bus_;
 
-		std::unordered_map<const Stop*, std::unordered_set<const Bus*>> bus_by_stop_;
+		std::unordered_map<StopPtr, std::unordered_set<BusPtr>> bus_by_stop_;
 
-		std::unordered_map<std::pair<const Stop*, const Stop*>, int, pair_stops_hasher> pair_stop_to_distance_;
+		std::unordered_map<std::pair<StopPtr, StopPtr>, int, pair_stops_hasher> pair_stop_to_distance_;
 	};
 }
