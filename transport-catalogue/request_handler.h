6e25e58b3e8dce01df7cd0cb6477c32dd7a2fe59
nickyul <156240952+nickyul@users.noreply.github.com>
2024-07-02 13:31:40 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,10 +1,9 @@
 #pragma once
-#include "domain.h"
 #include "transport_catalogue.h"
 #include "svg.h"
 #include "map_renderer.h"
-#include "geo.h"
 #include "json_reader.h"
+
 #include <algorithm>
 #include <optional>
 #include <unordered_set>
@@ -15,7 +14,7 @@ using namespace catalogue;
 class RequestHandler {
 public:
     // MapRenderer понадобится в следующей части итогового проекта
-    RequestHandler(const TransportCatalogue& db, const JsonReader& json_reader, RenderSettings settings);
+    RequestHandler(const TransportCatalogue& db, const JsonReader& json_reader, const MapRenderer& renderer);
 
     // Возвращает информацию о маршруте (запрос Bus)
     std::optional<BusStat> GetBusStat(const std::string_view& bus_name) const;
@@ -23,19 +22,11 @@ public:
     // Возвращает маршруты, проходящие через
     const std::unordered_set<const Bus*>* GetBusesByStop(const std::string_view& stop_name) const;
 
-    json::Document GetRequestDocument() const;
-
     void RenderMap(std::ostream& out) const;
 
 private:
     // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
     const TransportCatalogue& db_;
     const JsonReader& json_reader_;
-    const MapRenderer renderer_;
-
-    void MakeBusDict(json::Dict& r, BusStat stat, const json::Dict& request_map) const;
-
-    void MakeStopDict(json::Dict& r, const json::Dict& request_map) const;
-
-    void MakeMapDict(json::Dict& r, const json::Node& id) const;
+    const MapRenderer& renderer_;
 };
