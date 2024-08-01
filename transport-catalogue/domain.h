0b45a54afb1d59e791ed0808f4ef00e3a9c9eb7b
nickyul <156240952+nickyul@users.noreply.github.com>
2024-08-01 16:12:10 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include "geo.h"
+
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -15,11 +17,26 @@ struct Bus {
 	bool is_roundtrip;
 };
 
+using StopMap = std::unordered_map<std::string_view, const Stop*>;
+using BusMap = std::unordered_map<std::string_view, const Bus*>;
+using StopPtr = const Stop*;
+using BusPtr = const Bus*;
+
 struct BusStat {
 	size_t total_stops = 0;
 	size_t unique_stops = 0;
 	double route_length = 0.;
 	double curvature = 0;
+};
+
+struct RouteSettings {
+	double bus_wait_time = 0.;
+	double bus_velocity = 0.;
+};
+
+struct StopId {
+	size_t input_id = 0;
+	size_t output_id = 0;
 };
 
 using StopMap = std::unordered_map<std::string_view, const Stop*>;
