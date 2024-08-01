839b27e0a01e39c2029676c1994082107a3b769b
nickyul <156240952+nickyul@users.noreply.github.com>
2024-08-01 16:14:50 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 #include "map_renderer.h"
 #include "json_reader.h"
 #include "request_handler.h"
+#include "transport_router.h"
+
 using namespace std;
 using namespace catalogue;
 using namespace json;
@@ -12,15 +15,17 @@ int main() {
     TransportCatalogue catalogue;
     JsonReader json_reader(cin);
     json_reader.MakeCatalogue(catalogue);
-    RenderSettings settings = json_reader.ParseSettings();
 
+    RenderSettings settings = json_reader.ParseSettings();
     MapRenderer renderer(settings);
 
+    TransportRouter transport_router(catalogue, json_reader.GetRouteSettings());
+
+    
     RequestHandler request_handler(catalogue, json_reader, renderer);
     
-    Document doc_out = json_reader.GetRequestDocument(catalogue, renderer);
-
+    Document doc_out = json_reader.GetRequestDocument(catalogue, renderer, transport_router);
     Print(doc_out, cout);
-
+    
     return 0;
 }
